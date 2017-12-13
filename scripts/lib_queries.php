0e1ce82e860c17b202da2513865b31ef8c37d55d
pro-ject <pro-ject@mail.ru>
2017-12-13 20:25:03 +0300

    modify debug add debug msg

--- scripts/lib_queries.php
+++ scripts/lib_queries.php
@@ -20,61 +20,28 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-?>
-<?php
+
 require_once("config.php");
-require_once("libyate.php");
+//require_once("libyate.php");
 
 function query_to_array($query) {
-    /*
-    $res = query($query);
-    if(!$res) {
-        return array();
-    }
-    $array = array();
-    $i=0;
-    while ($row=$res->fetchRow()) {  
-        foreach ($row as $key=>$value){
-            $array[$i][$key]= $value;
-     };
-     $i++;
-    }
-    */
-    global $conn;
+    global $conn, $query_on, $type_debug;
     $array  = $conn->getAll($query);
-    Yate::Output("Executed: $query");
-    Yate::Output("Result:".json_encode($array)."\n");
-    //$res->free();
+    if($query_on) {
+        Yate::Output("Executed[$type_debug]: $query");
+        Yate::Output("Result[$type_debug]:".json_encode($array)."\n");
+    }
     return $array;
 }
 
 function query($query) {
-    global $conn, $query_on, $max_resets_conn;
+    global $conn, $query_on, $type_debug;
     global $dsn;
     $resets = 0;
-    while(true) {
-	if ($conn)
-        $res = $conn->query($query);/*
-        if(!$res) {
-            while(true) {
-                if($resets >= ($max_resets_conn-1)) {
-                    Yate::Output("Could not execute: $query\n");
-                    return null;
-                }
-                $resets++;
-                if (!$conn)
-                {
-                        $conn = DB::connect($dsn, true);
-                        break;
-                    sleep(1);
-                }else
-                    $resets = $max_resets_conn;
-            }
-        }else    */
-            break; 
-    } 
+    if ($conn)
+             $res = $conn->query($query);
     if($query_on)
-        Yate::Output("Executed: $query"."\n");
+        Yate::Output("Executed[$type_debug]: $query"."\n");
     return $res;
 }
 
@@ -91,20 +58,56 @@ function getCustomVoicemailDir($called) {
     $dir = "$vm_base/$last";
     if (!is_dir($dir)) {
         mkdir($dir,0750);
-        chown($dir,"apache");
+        //chown($dir,"www-data");
     }
     $dir = "$vm_base/$last/$alast/";
     if (!is_dir($dir)) {
         mkdir($dir,0750);
-        chown($dir,"apache");
+        //chown($dir,"www-data");
     }
     $dir = "$vm_base/$last/$alast/$called";
     if (!is_dir($dir)) {
         mkdir($dir,0750);
-        chown($dir,"apache");
+        //chown($dir,"www-data");
     }
     return $dir;
 }
 
+function chek_debug() {
+    global  $query_on, $type_debug;
+    //$type_debug = 'register';                           //one rules to ALL .php
+    $res = query_to_array("SELECT param, value FROM settings where description = '$type_debug'");
+    foreach ($res as $row) {       
+       if ($row["param"] == "debug")
+            set_debug($row["value"], $type_debug);
+       elseif ($row["param"] == "query") {
+            $query_on = $row["value"];
+            if ($query_on)
+                 Yate::Output(">>> Enabling query ".$type_debug.".php module");
+            else
+                 Yate::Output(">>> Disable query ".$type_debug.".php module");
+       }
+    }
+}
 
+function set_debug($enbl) {
+    global $type_debug, $debug_on;    
+    if ($enbl) {
+        $debug_on = true;
+        Yate::Debug(true);
+        Yate::Output(true);
+        Yate::Output(">>> Enabling debug ".$type_debug.".php module");
+    } else {
+        $debug_on = false;
+        Yate::Output(">>> Disable debug ".$type_debug.".php module");        
+        Yate::Output(false);
+        Yate::Debug(false);
+    }    
+}
+
+function debug($mess) {
+    global $type_debug, $debug_on;
+
+    Yate::Debug("[DEBUG ".$type_debug.".php]: " . $mess);
+}
 ?>
